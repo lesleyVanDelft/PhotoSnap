9753a0455709f74ea7c9c828a4485038939f9e86
Lesley van Delft <lesleyvandelft92@gmail.com>
2021-07-04 15:34:05 +0200

    started pricing page, first content section fully responsive

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Stories from './pages/Stories';
 import Features from './pages/Features';
+import Pricing from './pages/Pricing';
 
 function App() {
 	return (
@@ -11,6 +12,7 @@ function App() {
 					<Route path="/" exact component={Home} />
 					<Route path="/stories" component={Stories} />
 					<Route path="/features" component={Features} />
+					<Route path="/pricing" component={Pricing}></Route>
 				</Switch>
 			</div>
 		</Router>
---
+++ src/pages/Pricing.js
@@ -0,0 +1,45 @@
+import { useMediaQuery } from 'react-responsive';
+import Navbar from '../components/Navbar/Navbar';
+import HeroLarge from '../components/HeroLarge/HeroLarge';
+import HeroSmall from '../components/HeroSmall/HeroSmall';
+import HeroXS from '../components/HeroXS/HeroXS';
+import './Pricing.css';
+
+const Pricing = () => {
+	// React media queries
+	const tabletScreens = useMediaQuery({
+		minWidth: 768,
+		maxWidth: 1200,
+	});
+	const desktopScreens = useMediaQuery({
+		minWidth: 1200,
+	});
+	const smScreens = useMediaQuery({
+		maxWidth: 768,
+	});
+
+	return (
+		<main className="pricing">
+			<Navbar />
+			<section className="row m-0 p-0 flex-row-reverse">
+				<HeroSmall
+					src={
+						(smScreens ? '/images/pricing/mobile/hero.jpg' : undefined) ||
+						(tabletScreens ? '/images/pricing/tablet/hero.jpg' : undefined) ||
+						(desktopScreens ? '/images/pricing/desktop/hero.jpg' : undefined)
+					}
+					columns={'col-md-4 col-xl-7'}
+				/>
+				<HeroXS
+					heading={'Pricing'}
+					body={
+						'Create a your stories, Photosnap is a platform for photographers and visual storytellers. It’s the simple way to create and share your photos.'
+					}
+					columns={'col-md-8 col-xl-5'}
+				/>
+			</section>
+		</main>
+	);
+};
+
+export default Pricing;
